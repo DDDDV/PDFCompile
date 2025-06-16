a2981bc210ac2b039b1069ab175f98c50b9c14d4
liang.xu <liang.xu@mac>
2025-06-16 11:06:13 +0800

    修改git add

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd third_party
+
+unzip -o zlib-develop.zip
+
+unzip -o libtiff-master.zip
+
+unzip -o openjpeg-master.zip
+
+unzip -o libharu-libharu-v2.4.5-8-g555d4c0.zip
+
+unzip -o leptonica-master.zip
+
+unzip -o libpng-libpng16
